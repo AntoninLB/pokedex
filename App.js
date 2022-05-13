c44b4f1bc53767fd24329bfcecef6a08af741218
Lebeau Antonin <antonin.lebeau@itinnove.com>
2022-05-13 15:12:24 +0200

    add screen orientation feature

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
 import Home from "./src/views/Home";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
   return (
